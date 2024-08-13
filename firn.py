82e852dbc056787f6f5709c3edb91642aeeafee4
seccode <42663376+seccode@users.noreply.github.com>
2024-08-13 16:07:35 -0700

    Add files via upload

--- firn.py
+++ firn.py
@@ -1,68 +1,108 @@
-import ast
-from collections import defaultdict,Counter
-import json
+from collections import Counter,OrderedDict
+from concurrent.futures import ThreadPoolExecutor,as_completed
+import csv
+import time
+from tqdm import tqdm
 import zstandard as zstd
 
-def compress(s):
-    comp=zstd.ZstdCompressor()
-    sp="qqqqq"
-    while sp in s:
-        sp+="q"
-    words=s.split(" ")
-    x=[]
-    d=defaultdict(list)
+def get_symbols():
+    t=["e","t","o","n","i","h","s","r","d","l","u","m",",","w","c","f","g","y","p","b","'","v","k","-","M",'"',";","S","!","H","C","x","W","D","?"]
+    a=t[:]
+    for l0 in a:
+        for l1 in a:
+            t.append(l0+l1)
+    return t
+
+def compress(s,C,possible):
+    comp=zstd.ZstdCompressor(level=22)
+    if possible:
+        words=s.split(" ")
+        c=Counter(words).most_common()
+        d=OrderedDict()
+        i=0
+        j=0
+        t=get_symbols()
+        while i<len(t):
+            if len(c[j][0])==1 or \
+                (len(c[j][0])<=len(t[i])):
+                j+=1
+            else:
+                d[c[j][0]]=t[i]
+                i+=1
+                j+=1
+        y=set(d.values())
+        r=[]
+        for i,word in tqdm(enumerate(words),total=len(words)):
+            if word in d:
+                if i==0:
+                    r.append(d[word])
+                else:
+                    r.append(" "+d[word])
+            else:
+                if word in y:
+                    if i==0:
+                        r.append(C+word)
+                    else:
+                        r.append(" "+C+word)
+                else:
+                    if i==0:
+                        r.append(word)
+                    else:
+                        r.append(" "+word)
+        x="^".join(d.keys())+"`"+"".join(r)
+        c=comp.compress(x.encode("utf-8","replace"))
+    else:
+        c=comp.compress(s.encode("utf-8","replace"))
+    return c
+
+def decompress(c,C):
+    d=zstd.decompress(c).decode("utf-8","replace")
+    split_string=d.split("`",1)
+    dic=split_string[0].split("^")
+    text=split_string[1]
+    words=text.split(" ")
+    t=get_symbols()
+    d=OrderedDict()
+    for i,S in enumerate(t[:len(dic)]):
+        d[S]=dic[i]
+    j=set(d.keys())
+    res=[]
     for word in words:
-        if word=="":
-            x.append(chr(0))
+        if word[1:] in d and word[0]==C:
+            res.append(word[1:])
+        elif word in d:
+            res.append(d[word])
         else:
-            x.append(word[0])
-            d[word[0]].append(word)
-
-    r={}
-    e={}
-    for k,v in d.items():
-        mc=Counter(v).most_common()
-        g={}
-        l=[]
-        for i,m in enumerate(mc):
-            g[m[0]]=i
-            l.append(m[0])
-        r[k]=l
-        e[k]=g
-
-    k=[]
-    for word in words:
-        if word=="":
-            pass
-        else:
-            k.append(chr(e[word[0]][word]))
-
-    sc="".join(k)+sp+"".join(x)+sp+json.dumps(r,separators=(",",":"))
-    return comp.compress(sc.encode("utf-8","replace")),sp
-
-def decompress(b,sp):
-    k,x,r=zstd.decompress(b).decode("utf-8","replace").split(sp)
-    r=ast.literal_eval(r)
-    words=[]
-    j=0
-    for i in range(len(x)):
-        if x[i]==chr(0):
-            words.append("")
-        else:
-            words.append(r[x[i]][ord(k[j])])
-            j+=1
-    return " ".join(words)
+            res.append(word)
+    s=" ".join(res)
+    return s
 
 if __name__=="__main__":
     s=open("dickens",encoding="Windows-1252").read()
-    b,sp=compress(s)
-    f=open("compressed","wb")
+    comp=zstd.ZstdCompressor(level=22)
+    b=comp.compress(s.encode("utf-8","replace"))
+    f=open("1","wb")
     f.write(b)
     f.close()
-    _s=decompress(b,sp)
-    f=open("decompressed","w",encoding="Windows-1252")
-    f.write(_s)
+    t=get_symbols()
+    i=420
+    possible=True
+    while True:
+        found=False
+        try:
+            for S in t:
+                if " "+chr(i)+S+" " in s:
+                    found=True
+                    break
+        except:
+            possible=False
+        if not found:
+            break
+        i+=1
+    C=chr(i)
+    b=compress(s,C,possible)
+    f=open("2","wb")
+    f.write(b)
     f.close()
-
-
-
+    _s=decompress(b,C)
+    assert _s==s
