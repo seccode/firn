473d4765835ae448454ce24531ef21ec5b63f774
seccode <42663376+seccode@users.noreply.github.com>
2024-10-30 18:33:52 -0700

    Add files via upload

--- new.py
+++ new.py
@@ -8,70 +8,65 @@ def compress(s,comp):
     most_common_words=open("dict").read().split("\n")
     g=set(most_common_words)
 
+    d=defaultdict(set)
+    h={}
     words=s.split(" ")
     new_words=[]
-    d=defaultdict(set)
-    j=set([m[0] for m in Counter(s).most_common()][:30])
-    h={}
+    c=set([m[0] for m in Counter(s).most_common()][:20])
     x=[]
-    for word in tqdm(words):
-        if (word in g or word[:-1] in g or word[1:] in g) and any(c in word for c in j):
+    for i,word in tqdm(enumerate(words),total=len(words)):
+        if (word in g) and any(c in word for c in c):
             w=word
-            for _j in j:
-                w=w.replace(_j,"")
+            for _c in c:
+                w=w.replace(_c,"")
             if word in g:
                 if word not in h:
                     h[word]=len(d[w])
-                x.append(chr(h[word]+1))
                 d[w].add(word)
-            elif word[:-1] in g:
+                x.append(chr(h[word]+1))
+            else:
                 if word[:-1] not in h:
                     h[word[:-1]]=len(d[w])
-                x.append(chr(h[word[:-1]]+1))
                 d[w].add(word[:-1])
-            else:
-                if word[1:] not in h:
-                    h[word[1:]]=len(d[w])
-                x.append(chr(h[word[1:]]+1))
-                d[w].add(word[1:])
+                x.append(chr(h[word[:-1]]+1))
             new_words.append(w)
         elif word.count("\n")==1:
-            l=word.split("\n")
-            word0=l[0]
-            word1=l[0]
+            wi=word.split("\n")
+            word0=wi[0]
+            word1=wi[1]
             y=""
-            if (word0 in g or word0[:-1] in g) and any(c in word0 for c in j):
+            if (word0 in g) and any(c in word0 for c in c):
                 w=word0
-                for _j in j:
-                    w=w.replace(_j,"")
+                for _c in c:
+                    w=w.replace(_c,"")
                 if word0 in g:
                     if word0 not in h:
                         h[word0]=len(d[w])
-                    x.append(chr(h[word0]+1))
                     d[w].add(word0)
+                    x.append(chr(h[word0]+1))
                 else:
                     if word0[:-1] not in h:
                         h[word0[:-1]]=len(d[w])
-                    x.append(chr(h[word0[:-1]]+1))
                     d[w].add(word0[:-1])
+                    x.append(chr(h[word0[:-1]]+1))
                 y+=w
             else:
                 y+=word0
             y+="\n"
-            if (word1 in g or word1[:-1] in g) and any(c in word1 for c in j):
+            if (word1 in g) and any(c in word1 for c in c):
                 w=word1
-                for _j in j:
-                    w=w.replace(_j,"")
+                for _c in c:
+                    w=w.replace(_c,"")
                 if word1 in g:
                     if word1 not in h:
                         h[word1]=len(d[w])
-                    x.append(chr(h[word1]+1))
                     d[w].add(word1)
+                    x.append(chr(h[word1]+1))
                 else:
                     if word1[:-1] not in h:
                         h[word1[:-1]]=len(d[w])
-                    x.append(chr(h[word1[:-1]]+1))
                     d[w].add(word1[:-1])
+                    x.append(chr(h[word1[:-1]]+1))
                 y+=w
             else:
                 y+=word1
@@ -79,65 +74,76 @@ def compress(s,comp):
         else:
             new_words.append(word)
 
-    mc=[m[0] for m in Counter(new_words).most_common() if m[1]>5]
-    sym=[m[0] for m in Counter(s).most_common()][:60]
-    sym.remove(" ")
+    v=chr(0).join([
+        " ".join(new_words),
+        "".join(x),
+    ])
+    #return comp.compress(v.encode("utf-8"))
+    return zlib.compress(v.encode("utf-8","replace"),level=-1)
+
+def decompress(b):
+    d_keys,new_new_words,x,t,m,z,q,sym=zlib.decompress(b).decode("utf-8").split(chr(600))
+    new_new_words=new_new_words.replace(chr(0),"    ").split(" ")
+    sym=list(sym)
+    z=list(z)
+    q=list(q)
     t=sym[:]
     for l0 in t:
         for l1 in t:
             sym.append(l0+l1)
+    mc=d_keys.split(" ")
     d={}
     j=0
     for i in range(len(mc)):
         if j==len(sym):
             break
         if len(mc[i])>len(sym[j]):
-            d[mc[i]]=sym[j]
+            d[sym[j]]=mc[i]
             j+=1
-    new_new_words=[]
-    for word in new_words:
-        if word in d:
-            new_new_words.append(d[word])
-        elif word[:-1] in d:
-            new_new_words.append(d[word[:-1]]+word[-1])
+    new_words=[]
+    l=0
+    r=0
+    for i,word in enumerate(new_new_words):
+        if len(z)>0 and chr(i-l)==z[0]:
+            new_words.append(word)
+            z.pop(0)
+            l=i
+        elif len(q)>0 and chr(i-r)==q[0]:
+            new_words.append(d[word[:-1]]+word[-1])
+            q.pop(0)
+            r=i
+        elif word in d:
+            new_words.append(d[word])
         else:
-            new_new_words.append(word)
-    v=chr(256).join([
-        " ".join(d.keys()),
-        " ".join(new_new_words).replace("    ",chr(0)),
-        "".join(x)
-    ])
-    #return comp.compress(v.encode("utf-8","replace"))
-    return zlib.compress(v.encode("utf-8","replace"),level=-1)
-
-def decompress(b):
-    new_words=zlib.decompress(b).decode("utf-8")
+            new_words.append(word)
     words=[]
-    return " ".join(words)
+    return new_words
 
 if __name__=="__main__":
     # Read dickens
-    s=open("dickens",encoding="latin-1").read()[:1_000_000]
+    s=open("dickens",encoding="latin-1").read()[:10_000]
 
     f=open("s","w")
     f.write(s)
     f.close()
 
     comp=zstd.ZstdCompressor(level=22)
-    #_b=comp.compress(s.encode("utf-8","replace"))
-    _b=zlib.compress(s.encode("utf-8"),level=9)
+    #_b=comp.compress(s.encode("utf-8"))
+    _b=zlib.compress(s.encode("utf-8"),level=-1)
 
-    # Compress with our custom algorithm
+    # Compress with the custom algorithm
     b=compress(s,comp)
 
     # Print results
     print("\n************************************************")
     print("Compressed "+str(len(s))+" bytes to "+str(len(b))+" bytes")
-    print(str(round(100*(len(_b)-len(b))/len(_b),2))+"% improvement over zstd")
+    print(str(round(100*(len(_b)-len(b))/len(_b),2))+"% improvement")
     print("************************************************\n")
 
-    # Decompress with our custom algorithm
+    # Decompress with the custom algorithm
     _s=decompress(b)
+    assert _s==n
+    a
 
     # Assert equality
     assert _s==s
